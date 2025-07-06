3ee1c69afa229e800b556248981f1ab19c8d43d0
Adam <akhrorkuloww@gmail.com>
2025-07-06 22:00:00 +0500

    Save data and generate HTML for payment summary

--- scripts/checkot/orderSummary.js
+++ scripts/checkot/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProductById } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js'
+import { deliveryOptions, getDeliveryOptionById } from '../../data/deliveryOptions.js'
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days')
@@ -15,27 +15,11 @@ export function renderOrderSummary(){
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProductById(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
-
-    if (!deliveryOption) {
-      console.warn(`No delivery option found for id: ${deliveryOptionId}`);
-      return; // Skip this cart item
-    }
+    const deliveryOption = getDeliveryOptionById(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,4 @@
 import { renderOrderSummary } from "./checkot/orderSummary.js";
-
+import { renderPaymentSummary } from "./checkot/paymentSummary.js";
 renderOrderSummary();
+renderPaymentSummary();
