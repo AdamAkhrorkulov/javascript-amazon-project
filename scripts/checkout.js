edb71df09f87dd87ec837dce239a9cb37b1b033c
Adam <akhrorkuloww@gmail.com>
2025-07-07 18:08:32 +0500

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkot/orderSummary.js";
 import { renderPaymentSummary } from "./checkot/paymentSummary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 renderOrderSummary();
 renderPaymentSummary();
 
