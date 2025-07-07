928d92e0a982c809a597d593a55e824c2a4a1388
Adam <akhrorkuloww@gmail.com>
2025-07-07 16:26:34 +0500

    Create oop versio of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import { renderOrderSummary } from "./checkot/orderSummary.js";
 import { renderPaymentSummary } from "./checkot/paymentSummary.js";
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
+
