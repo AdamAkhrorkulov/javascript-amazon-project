adf8d5f1d01775b2d936ab6380509320767f4dfe
Adam <akhrorkuloww@gmail.com>
2025-07-07 19:53:04 +0500

    Convert products in to classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkot/orderSummary.js";
 import { renderPaymentSummary } from "./checkot/paymentSummary.js";
-import '../data/cart-class.js';
+// import '../data/cart-class.js';
 renderOrderSummary();
 renderPaymentSummary();
 
